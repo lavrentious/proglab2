6809a9fc860d2672de1f1f02f2ad794f17ee1c71
lavrentious <lavrent0@yandex.ru>
2023-10-18 10:13:55 +0300

    fix: inherit from previous evolution

--- app/src/main/java/ru/lavrent/lab2/pokemon/Cacturne.java
+++ app/src/main/java/ru/lavrent/lab2/pokemon/Cacturne.java
@@ -1,16 +1,13 @@
 package ru.lavrent.lab2.pokemon;
 
-import ru.ifmo.se.pokemon.Pokemon;
 import ru.ifmo.se.pokemon.Type;
 import ru.lavrent.lab2.moves.Charge;
-import ru.lavrent.lab2.moves.DoubleEdge;
-import ru.lavrent.lab2.moves.Endeavor;
 
-public class Cacturne extends Pokemon {
+public class Cacturne extends Cacnea {
   public Cacturne(String name, int level) {
     super(name, level);
     this.setStats(70, 115, 60, 115, 60, 55);
     this.setType(Type.GRASS, Type.DARK);
-    this.setMove(new Endeavor(), new DoubleEdge(), new Charge());
+    this.addMove(new Charge());
   }
 }
